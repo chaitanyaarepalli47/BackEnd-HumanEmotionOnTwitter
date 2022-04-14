572c38dc0a1b3c87d585f90fe6bbbf25b5213a46
YOUR_USERNAME <89421055+chaitanyaarepalli47@users.noreply.github.com>
2022-04-14 14:47:53 -0600

    adding

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.logger());
-
 app.listen(
     port,
     () => console.log('its live')
@@ -14,14 +12,31 @@ app.get('/', (req, res) => {
 });
 
 // not working properly
-const Pool = require("pg").Pool;
-require("dotenv").config();
-const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+// const Pool = require("pg").Pool;
+// require("dotenv").config();
+// const isProduction = process.env.NODE_ENV === "production";
+// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const pool = new Pool({
+//     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     },
+// });
+// module.exports = pool;
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? true : false
 });
-module.exports = pool;
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
