55d2b5f19e17d5e6ad48803efde0719ea4c936ad
YOUR_USERNAME <89421055+chaitanyaarepalli47@users.noreply.github.com>
2022-04-14 13:21:39 -0600

    connecting to db

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const port = process.env.PORT || 8080;
+app.use(express.logger());
 
 app.listen(
-    PORT,
+    port,
     () => console.log('its live')
 )
 
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: 'asklls',
-        size: 'large'
-    })
+app.get('/', (req, res) => {
+    res.send(`Hello World ${port}`);
+});
+
+// not working properly
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM login_details', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
 });
