2aa153772c96c7a44f15e1333705956131830bde
YOUR_USERNAME <89421055+chaitanyaarepalli47@users.noreply.github.com>
2022-04-14 13:56:55 -0600

    trying to connect to db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+
 app.use(express.logger());
 
 app.listen(
@@ -13,12 +14,14 @@ app.get('/', (req, res) => {
 });
 
 // not working properly
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM login_details', function(err, result) {
-    done();
-    if(err) return console.error(err);
-    console.log(result.rows);
-  });
+const Pool = require("pg").Pool;
+require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
+module.exports = pool;
