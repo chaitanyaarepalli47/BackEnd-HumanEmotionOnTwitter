c5f6995229af461255fc326cde14ba07e3268307
YOUR_USERNAME <89421055+chaitanyaarepalli47@users.noreply.github.com>
2022-04-14 21:09:41 -0600

    resolved errors in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+require('dotenv').config()
 
 app.listen(
     port,
@@ -12,31 +13,16 @@ app.get('/', (req, res) => {
 });
 
 // not working properly
-// const Pool = require("pg").Pool;
-// require("dotenv").config();
-// const isProduction = process.env.NODE_ENV === "production";
-// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-// const pool = new Pool({
-//     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-//     ssl: {
-//         rejectUnauthorized: false,
-//     },
-// });
-// module.exports = pool;
+const Pool = require("pg").Pool;
+require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+module.exports = pool;
 
-// const { Client } = require('pg');
-
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: process.env.DATABASE_URL ? true : false
-// });
-
-// client.connect();
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
+// const { Pool } = require('pg');
---
+++ knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tablename: ''
+        }
+    }
+}
